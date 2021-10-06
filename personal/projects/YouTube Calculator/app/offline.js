74053536355877723cd05c1d851b4e55d97b02f3
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-10-05 21:48:41 -0300

    Very welcome updates that shall be applied years ago

---
+++ personal/projects/YouTube Calculator/app/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1615583707,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/calc2-sheet0.png",
+		"images/div.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
